e25aede543703be08645fe3bf78be5404585f0a1
servomouse <servomouse@protonmail.com>
2025-07-09 21:50:25 -0600

    Make both C and python tests work

--- neuronet/tests/python_tests.cmake
+++ neuronet/tests/python_tests.cmake
@@ -1,16 +1,20 @@
-# find_package(Python3 COMPONENTS Development REQUIRED)
 
-# # Should be set by find_package(Python3..., if it isn't, set it manually
-# if(NOT Python3_EXECUTABLE)
-#     set(Python3_EXECUTABLE "python")  # or "python3" depending on your system
-# endif()
 set(Python3_EXECUTABLE "${CMAKE_SOURCE_DIR}/myenv/Scripts/python")
 
+message(STATUS "CMAKE_SOURCE_DIR: ${CMAKE_SOURCE_DIR}")
+
+# Define the test command
+set(TEST_COMMAND ${Python3_EXECUTABLE} -m unittest discover -s ${CMAKE_SOURCE_DIR}/tests -p *.py)
+
+# Use a consistent name for the test
+set(TEST_NAME "python_tests")
+
 # Add a custom target for Python tests
-add_custom_target(py_tests
-    COMMAND ${Python3_EXECUTABLE} -m unittest discover -s . -p "*.py"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+add_custom_target(${TEST_NAME}
+    COMMAND ${TEST_COMMAND}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tests
 )
 
 # Add the custom target to the test suite
-add_test(NAME py_tests COMMAND ${Python3_EXECUTABLE} -m unittest discover -s . -p "*.py")
+add_test(NAME ${TEST_NAME} COMMAND ${TEST_COMMAND})
+set_tests_properties(${TEST_NAME} PROPERTIES WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tests)
