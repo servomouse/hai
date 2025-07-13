0e68f5f58e155044612e9130fed679131d580647
servomouse <servomouse@protonmail.com>
2025-07-13 15:19:27 -0600

    Update the tests configuration to work with python tests more efficiently

--- neuronet/tests/python_tests.cmake
+++ neuronet/tests/python_tests.cmake
@@ -1,22 +1,22 @@
 
 set(Python3_EXECUTABLE "${CMAKE_SOURCE_DIR}/myenv/Scripts/python")
 
-# message(STATUS "CMAKE_SOURCE_DIR: ${CMAKE_SOURCE_DIR}")
-
-# Define the test command
-set(TEST_COMMAND ${Python3_EXECUTABLE} -m unittest discover -s ${CMAKE_SOURCE_DIR}/tests -p *.py)
-
-# Use a consistent name for the test
-set(TEST_NAME "python_tests")
-
-# Add a custom target for Python tests
-add_custom_target(${TEST_NAME}
-    COMMAND ${TEST_COMMAND}
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tests
+# Manually specify the list of Python test files
+set(PYTHON_TEST_FILES
+    linear_net_instantiation
+    linear_net_evolution
 )
 
-add_dependencies(${TEST_NAME} network_shared)
+foreach(TEST_NAME ${PYTHON_TEST_FILES})
+    set(TEST_COMMAND ${Python3_EXECUTABLE} -m unittest ${TEST_NAME})
 
-# Add the custom target to the test suite
-add_test(NAME ${TEST_NAME} COMMAND ${TEST_COMMAND})
-set_tests_properties(${TEST_NAME} PROPERTIES WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tests)
+    add_custom_target(${TEST_NAME}
+        COMMAND ${TEST_COMMAND}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tests
+    )
+    add_dependencies(${TEST_NAME} network_shared)
+
+    # Add the custom target to the test suite
+    add_test(NAME ${TEST_NAME} COMMAND ${TEST_COMMAND})
+    set_tests_properties(${TEST_NAME} PROPERTIES WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/tests)
+endforeach()
