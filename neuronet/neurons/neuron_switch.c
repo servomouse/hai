0974ffeb880679cddafadc9c55f5dcd3c748b922
servomouse <servomouse@protonmail.com>
2025-08-08 21:08:08 -0600

    Add the switch neuron template

---
+++ neuronet/neurons/neuron_switch.c
@@ -0,0 +1,43 @@
+#include "neuron.h"
+
+void neuron_pattern_create(neuron_t *n, uint32_t num_inputs) {
+    RAISE("Error: Pattern neuron not implemented!\n");
+}
+
+double neuron_pattern_get_output(neuron_t *n) {
+    RAISE("Error: Pattern neuron not implemented!\n");
+}
+
+char *neuron_pattern_get_coeffs(neuron_t *n) {
+    RAISE("Error: Pattern neuron not implemented!\n");
+}
+
+void neuron_pattern_set_coeffs(neuron_t *n, double *values) {
+    RAISE("Error: Pattern neuron not implemented!\n");
+}
+
+/* MUTATIONS */
+
+// The opposite is neuron_rollback
+void neuron_pattern_stash_state(neuron_t * n) {
+    RAISE("Error: Pattern neuron not implemented!\n");
+}
+
+void neuron_pattern_mutate(neuron_t * n, double mutation_step) {
+    RAISE("Error: Pattern neuron not implemented!\n");
+}
+
+// The opposite is neuron_stash_state
+void neuron_pattern_rollback(neuron_t * n) {
+    RAISE("Error: Pattern neuron not implemented!\n");
+}
+
+/* BACKPROPAGATION */
+
+void neuron_pattern_backpropagate(neuron_t *n, backprop_error_t *errors, uint32_t self_idx) {
+    RAISE("Error: Pattern neuron not implemented!\n");
+}
+
+void neuron_pattern_backprop_update_weights(neuron_t *n, double learning_rate) {
+    RAISE("Error: Pattern neuron not implemented!\n");
+}
