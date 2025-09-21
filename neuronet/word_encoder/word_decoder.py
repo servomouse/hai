7d0c90a26ae00ae21a3bb0b22bdd70f511c9bca6
servomouse <servomouse@protonmail.com>
2025-09-20 21:07:38 -0600

    Implement decoder training algorithm

--- neuronet/word_encoder/word_decoder.py
+++ neuronet/word_encoder/word_decoder.py
@@ -17,53 +17,77 @@ LETTER_WIDTH = 5
 SPARSE_PARAM = 5
 
 
-def get_decoder_error(decoder, dataset, to_print=False):
-    pass
+def word_representation(word):
+    output = []
+    for letter in word:
+        output.append(alphabet[letter])
+    output.append(alphabet['EOL'])
+    return output
+
+
+def get_vector_error(output_vec, target_vec):
+    error = 0
+    for i in range(len(output_vec)):
+        error += abs(output_vec[i] - target_vec[i])
+    return error / len(output_vec)
+
+
+def get_decoder_error(decoder:NetworkInterface, dataset, to_print=False):
+    error = 0
+    for d in dataset:
+        input_vector = d[0]
+        outputs = d[1]
+        curr_output = [0 for _ in range(LETTER_WIDTH)] + [input_vector[i] for i in range(ENCODED_WIDTH)]
+        local_error = 0
+        for i in range(len(outputs)):
+            curr_output = decoder.get_outputs(curr_output[LETTER_WIDTH:], LETTER_WIDTH + ENCODED_WIDTH)
+            local_error += get_vector_error(curr_output[:LETTER_WIDTH], outputs[i])
+        error += local_error / len(outputs)
+    return error / len(dataset)
 
 
 def train_decoder(decoder, dataset):
 
-    min_error, _ = get_decoder_error(decoder, dataset, to_print=True)
+    min_error = get_decoder_error(decoder, dataset, to_print=True)
     print(f"Initial error: {min_error}")
-    no_duplicates = False
     for t in range(100):
-        for lap in range(10000):
+        for lap in range(100):
             print(f"\r{t = }, {lap = }", end='')
-            if no_duplicates:
-                break
             decoder.mutate(0.1)
-            new_error, has_duplicates = get_decoder_error(decoder, dataset)
-            if not has_duplicates:
-                no_duplicates = True
+            new_error = get_decoder_error(decoder, dataset)
             if new_error > min_error:
                 decoder.rollback()
             elif new_error < min_error:
                 min_error = new_error
                 print(f", {min_error = }")
-        if no_duplicates:
-            break
 
-        decoder.network_save_coeffs("encoder_coeffs.txt")
+        decoder.network_save_coeffs("decoder_coeffs.txt")
     print(f"\tFinal error: {get_decoder_error(decoder, dataset, to_print=True)}")
 
 
 def main():
     decoder_arch = get_net_arch([
         {"size": ENCODED_WIDTH, "type": "input",  "inputs": None},
-        {"size": ENCODED_WIDTH, "type": "input",  "inputs": None},
+        # {"size": ENCODED_WIDTH, "type": "input",  "inputs": None},
         {"size": 8,  "type": "hidden", "inputs": [0]},
         {"size": 8,  "type": "hidden", "inputs": [1]},
-        {"size": 8,  "type": "hidden", "inputs": [2, 3]},
-        {"size": LETTER_WIDTH + ENCODED_WIDTH,  "type": "output", "inputs": [4]},
+        {"size": 8,  "type": "hidden", "inputs": [2]},
+        {"size": LETTER_WIDTH + ENCODED_WIDTH,  "type": "output", "inputs": [3]},
     ])
     # save_arch_to_file(decoder_arch, "decoder_arch.txt")
 
     dll_loader = LoaderIface()
     decoder = NetworkInterface(get_network_arch(**decoder_arch), dll_loader)
-    # decoder.network_restore_coeffs("decoder_coeffs.txt")
+    decoder.network_restore_coeffs("decoder_coeffs.txt")
+
     encoder = load_encoder()
 
-    dataset = [[sparse_encode_word(encoder, w), w] for w in random.sample(english_words, 1000)]
+    dataset = [[sparse_encode_word(encoder, w), word_representation(w), w] for w in random.sample(english_words, 10000)]
+
+    # for d in dataset:
+    #     print(d[0])
+    #     print(d[1])
+    #     print(d[2], "\n")
 
     train_decoder(decoder, dataset)
 
