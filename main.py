c91161c758a294525fd1f56efbe2ad5d24fb795d
servomouse <servomouse@protonmail.com>
2025-12-25 22:56:50 -0600

    Implementing LTM

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import queue
+import os.path
+import json
+
+
 #---------------------------------------
 # Basic elements
 #---------------------------------------
@@ -59,6 +64,8 @@ class Architect:
 
 
 class LongTermMemory:
+    """ The place where Global Graph is stored and the Architect works"""
+    GLOBAL_GRAPH_FILE_PATH = "ltm_graph.json"
     _instance = None
 
     # Is a singleton
@@ -68,7 +75,27 @@ class LongTermMemory:
         return cls._instance
 
     def __init__(self):
+        self.LTM_queue = queue.Queue()
+        if os.path.isfile(self.GLOBAL_GRAPH_FILE_PATH):
+            with open(self.GLOBAL_GRAPH_FILE_PATH) as f:
+                self.memory_graph = json.loads(f.read())
+        else:
+            self.memory_graph = {}
+
+    def add_to_queue(self, graph):
+        self.LTM_queue.put(graph)
+    
+    def _get_from_queue(self):
+        g = self.LTM_queue.get()
+        self.LTM_queue.task_done()
+        return g
+    
+    def _update_graph(self, graph):
         pass
+
+    def rebuild_graph(self):
+        while not self.LTM_queue.empty():
+            self._update_graph(self._get_from_queue())
 
 
 class ShortTermMemory:
