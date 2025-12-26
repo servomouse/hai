6c3f4d94a49dc64914bac3c5b9565c9421a5aee3
servomouse <servomouse@protonmail.com>
2025-12-25 22:38:47 -0600

    Add singletones, implementing the Node

--- main.py
+++ main.py
@@ -2,13 +2,49 @@
 # Basic elements
 #---------------------------------------
 class Node:
-    def __init__(self, vector, network):
+    def __init__(self, vector, network, LTM):
         self.vector = vector
         self.network = network
+        self.LTM = LTM
+        self.input_links = []
+        self.output_links = []
+    
+    def add_input_link(self, new_link):
+        if new_link not in self.input_links:
+            self.input_links.append(new_link)
+    
+    def remove_input_link(self, link):
+        if link in self.input_links:
+            # May contain only one entry of the link
+            self.input_links.remove(link)
+
+    def add_new_output_link(self, new_link):
+        if new_link not in self.output_links:
+            self.output_links.append(new_link)
+
+    def remove_new_output_link(self, link):
+        if link in self.output_links:
+            # May contain only one entry of the link
+            self.output_links.remove(link)
 
 
 class GraphBuilder:
     def __init__(self):
+        self.graph = None
+
+    def create_new_graph(self):
+        pass
+
+    def add_node(self):
+        pass
+
+    def move_graph_to_STM(self):
+        pass
+
+    def get_graph_to_STM(self):
+        pass
+
+    def move_graph_to_LTM(self):
         pass
 
 
@@ -23,15 +59,39 @@ class Architect:
 
 
 class LongTermMemory:
+    _instance = None
+
+    # Is a singleton
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(LongTermMemory, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self):
         pass
 
 
 class ShortTermMemory:
+    _instance = None
+
+    # Is a singleton
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(ShortTermMemory, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self):
         pass
 
 
 class ThinkingCore:
+    _instance = None
+
+    # Is a singleton
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(ThinkingCore, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self):
         pass
