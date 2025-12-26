3a326bd70ee19be555664276598356e60291f8f7
servomouse <servomouse@protonmail.com>
2025-12-25 22:28:00 -0600

    Implementing high-level arch

--- main.py
+++ main.py
@@ -1,45 +1,37 @@
-from inputs import InputHub
-from outputs import OutputHub
+#---------------------------------------
+# Basic elements
+#---------------------------------------
+class Node:
+    def __init__(self, vector, network):
+        self.vector = vector
+        self.network = network
 
 
-class Memory:
+class GraphBuilder:
     def __init__(self):
         pass
 
-class Scratchpad:
+
+class Architect:
     def __init__(self):
         pass
+
+
+#---------------------------------------
+# Basic locations
+#---------------------------------------
+
+
+class LongTermMemory:
+    def __init__(self):
+        pass
+
+
+class ShortTermMemory:
+    def __init__(self):
+        pass
+
 
 class ThinkingCore:
     def __init__(self):
         pass
-
-
-class Brain:
-    def __init__(self):
-        self.inputs = {
-            "text": [],
-            "image": None
-        }
-        self.ihub = InputHub()
-        self.scrpad = Scratchpad()
-        self.memory = Memory()
-        self.tcore = ThinkingCore()
-        self.ohub = OutputHub(self.ihub)
-
-    def tick(self):
-        pass
-
-    def text_input(self, text):
-        pass
-
-    def text_output(self, text):
-        pass
-
-    def image_input(self, image):
-        pass
-
-    def move(self, value):
-        action = (value >> 8) & 0xFF
-        param = value & 0xFF
-        self.do_motion(action, param)
